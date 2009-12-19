b1f570eeb6e4b51be53eac78180eed8074df1cc6
pierric <pierric@matrix.localdomain>
2009-12-20 03:34:08 +0800

    OpenGL TextureObject

--- BLP.hs
+++ BLP.hs
@@ -1,10 +1,13 @@
 module BLP where
 
 import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as SBS
 import Text.Printf
 import Control.Exception
 import Data.Binary
 import Data.Array
+import Data.Binary.Get
+import Data.Word
 import Control.Monad(ap,liftM3)
 import Utils
 
@@ -12,19 +15,22 @@ data CTYPE = DXT1
            | DXT3
            | DXT5
            deriving Show
-data BLP   = BLPp{ blp_palette_    :: Array Int Int
+data BLP   = BLPp{ blp_name        :: String
+                 , blp_palette_    :: Array Int Word32
                  , blp_size_       :: (Int,Int)
                  , blp_alpha_bits_ :: Int
-                 , blp_raw_        :: [BS.ByteString] 
+                 , blp_raw_        :: [SBS.ByteString] 
                  }
-           | BLPc{ blp_type_ :: CTYPE
-                 , blp_size_ :: (Int,Int)
-                 , blp_raw_  :: [BS.ByteString]
+           | BLPc{ blp_name        :: String
+                 , blp_type_       :: CTYPE
+                 , blp_size_       :: (Int,Int)
+                 , blp_raw_        :: [SBS.ByteString]
                  }
 
 data Header   = Header{ id_     :: String
                       , typ_    :: Int
                       , attr_   :: (Int,Int,Int)
+                      , mipmap_ :: Bool
                       , width_  :: Int
                       , height_ :: Int
                       , offset_ :: [Int]
@@ -37,19 +43,18 @@ newBLP fpath = do
   let hdr = decode archive :: Header
   assert (id_ hdr == "BLP2" && typ_ hdr == 1) (return ())
   let sz       = (width_ hdr, height_ hdr)
-  let palette  = listArray (0,255) $ getBunchOf 256 getUInt (BS.drop 148 archive)
-  let dat base = map (\(o,s) -> BS.take (fromIntegral s) (BS.drop (fromIntegral o) base)) $ 
-                 filter (\(o,s) -> o>0&&s>0) $ 
-                 zip (offset_ hdr) (sizes_ hdr)
+      palette  = listArray (0,255) $ getBunchOf 256 getWord32le (BS.drop 148 archive)
+      dat      = map (\(o,s) -> SBS.concat $ BS.toChunks $ BS.take (fromIntegral s) $ BS.drop (fromIntegral o) archive)
+                     (filter (\(o,s) -> o>0&&s>0) $ zip (offset_ hdr) (sizes_ hdr))
   case (attr_ hdr) of
-    (1,0,_) -> return $ BLPp palette sz 0 (dat (BS.drop 1172 archive))
-    (1,1,_) -> return $ BLPp palette sz 1 (dat (BS.drop 1172 archive))
-    (1,8,_) -> return $ BLPp palette sz 8 (dat (BS.drop 1172 archive))
-    (2,0,_) -> return $ BLPc DXT1 sz (dat (BS.drop 148 archive))
-    (2,1,_) -> return $ BLPc DXT1 sz (dat (BS.drop 148 archive))
-    (2,4,1) -> return $ BLPc DXT3 sz (dat (BS.drop 148 archive))
-    (2,8,1) -> return $ BLPc DXT3 sz (dat (BS.drop 148 archive))
-    (2,8,7) -> return $ BLPc DXT5 sz (dat (BS.drop 148 archive))
+    (1,0,_) -> return $ BLPp fpath palette sz 0 dat
+    (1,1,_) -> return $ BLPp fpath palette sz 1 dat
+    (1,8,_) -> return $ BLPp fpath palette sz 8 dat
+    (2,0,_) -> return $ BLPc fpath DXT1 sz dat
+    (2,1,_) -> return $ BLPc fpath DXT1 sz dat
+    (2,4,1) -> return $ BLPc fpath DXT3 sz dat
+    (2,8,1) -> return $ BLPc fpath DXT3 sz dat
+    (2,8,7) -> return $ BLPc fpath DXT5 sz dat
     (a,b,c) -> assert False (do putStrLn $ printf "Unknown blp format:%d,%d,%d" a b c 
                                 return undefined)
 
@@ -57,6 +62,7 @@ instance Binary Header where
     get   = return Header `ap` (sequence [getCChar,getCChar,getCChar,getCChar])
                           `ap` getUInt
                           `ap` liftM3 (,,) getUChar getUChar getUChar
+                          `ap` (fmap (>0) getUChar)
                           `ap` getUInt `ap` getUInt
                           `ap` (sequence $ replicate 16 getUInt)
                           `ap` (sequence $ replicate 16 getUInt)
---
+++ GL/Texture.hs
@@ -0,0 +1,79 @@
+module GL.Texture where
+
+import Data.Bits
+import qualified Data.ByteString as BS
+import Data.ByteString.Internal
+import Data.Word
+import Data.Array(Array,(!))
+import Control.Monad(forM_)
+import Foreign.Ptr(Ptr,plusPtr)
+import Foreign.Storable(poke)
+import Foreign.Marshal.Array(allocaArray)
+import Foreign.ForeignPtr
+import qualified Graphics.Rendering.OpenGL.GL as GL
+import qualified Graphics.Rendering.OpenGL.Raw as GLR
+
+import BLP
+
+data Texture = Texture{ -- tx_name_   :: String
+                        tx_type_   :: TextureType
+                      , tx_object_ :: GL.TextureObject 
+                      }
+
+data TextureType = TEX_TYPE_1D
+                 | TEX_TYPE_2D
+                 | TEX_TYPE_3D
+                 | TEX_TYPE_CUBE_MAP
+
+newTexture :: TextureType -> BLP -> IO Texture
+newTexture typ raw = do
+  [texture] <- GL.genObjectNames 1
+  GL.textureBinding (glTextureType typ) GL.$= Just texture
+  case raw of
+    BLPp _ pal size@(w,h) ab raw -> allocaArray (w*h) (setupLayerP pal size ab raw)
+    BLPc _ cty size@(w,h) raw    -> setupLayerC cty size raw
+  return (Texture {--(blp_name raw)--} typ texture)
+
+    where fix :: Word32 -> Word32 -> Word32 
+          fix v a = shiftR (v .&. 0x00FF0000) 16 .|. (v .&. 0x0000FF00) .|. shiftL (v .&. 0x000000FF) 16 .|. shiftL a 24
+
+          alpha :: Int -> BS.ByteString -> [Word32]
+          alpha 0 raw = repeat 0xff
+          alpha 1 raw = concatMap (\r -> map (\i -> if testBit r i then 0xff else 0) [0..7]) $ BS.unpack raw
+          alpha 8 raw = map fromIntegral (BS.unpack raw)
+
+          halve :: (Int,Int) -> [(Int,Int)]
+          halve (1,1) = repeat (1,1)
+          halve (1,n) = (1,n) : halve (1, div n 2)
+          halve (m,1) = (m,1) : halve (div m 2, 1)
+          halve (m,n) = (m,n) : halve (div m 2, div n 2)
+
+          setupLayerP :: Array Int Word32 -> (Int,Int) -> Int -> [BS.ByteString] -> Ptr Word32 -> IO ()
+          setupLayerP pal (w,h) ab raw buf =
+            forM_ (zip3 [1..] (halve (w,h)) raw) (\(l,(w',h'),r) -> do
+              let s = w'*h'
+              forM_ (zip3 [1..s] (BS.unpack r) (alpha ab (BS.drop s r))) (\(o,i,a) ->
+                poke (buf `plusPtr` o) (fix (pal ! fromIntegral i) a))
+              GL.texImage2D Nothing GL.NoProxy
+                            l GL.RGBA8 (GL.TextureSize2D (fromIntegral w') (fromIntegral h')) 0 
+                            (GL.PixelData GL.RGBA GL.UnsignedByte buf) )
+
+          setupLayerC :: CTYPE -> (Int,Int) -> [BS.ByteString] -> IO ()
+          setupLayerC ctype (w,h) raw = 
+            forM_ (zip3 [1..] (halve (w,h)) raw) (\(l,(w',h'),r) -> do
+              let (buf,lb,hb) = toForeignPtr r
+              withForeignPtr buf (\pb ->
+                GL.compressedTexImage2D Nothing GL.NoProxy
+                                        l (GL.TextureSize2D (fromIntegral w') (fromIntegral h')) 0
+                                        (GL.CompressedPixelData (glCompressedTextureFormat ctype) 
+                                                                (fromIntegral $ hb - lb) 
+                                                                pb)))
+
+          glTextureType TEX_TYPE_1D = GL.Texture1D
+          glTextureType TEX_TYPE_2D = GL.Texture2D
+          glTextureType TEX_TYPE_3D = GL.Texture3D
+          glTextureType TEX_TYPE_CUBE_MAP = GL.TextureCubeMap
+
+          glCompressedTextureFormat DXT1 = GL.CompressedTextureFormat GLR.gl_COMPRESSED_RGBA_S3TC_DXT1 
+          glCompressedTextureFormat DXT3 = GL.CompressedTextureFormat GLR.gl_COMPRESSED_RGBA_S3TC_DXT3
+          glCompressedTextureFormat DXT5 = GL.CompressedTextureFormat GLR.gl_COMPRESSED_RGBA_S3TC_DXT5
